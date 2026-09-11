1531c7d4c23742ec01ba4c841c265cd1e498a78e
Greg Fee <fee.gregory@yahoo.com>
2026-09-11 09:17:15 -1000

    establish product and process language packages

--- src/powdrr_lift/core/effective_contract.py
+++ src/powdrr_lift/core/effective_contract.py
@@ -8,7 +8,7 @@ from collections.abc import Mapping
 from dataclasses import dataclass
 from typing import Any
 
-from powdrr_lift.core.intent import IntentClause, IntentSource, IntentStore
+from powdrr_lift.product.intent import IntentClause, IntentSource, IntentStore
 
 
 @dataclass(frozen=True, slots=True)
